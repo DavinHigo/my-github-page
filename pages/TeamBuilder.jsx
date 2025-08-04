c0e0c73d1099de740b40a4ec9e42920fdc49b0bd
Davin Higo <davinhigo@gmail.com>
2025-08-03 19:56:36 -0700

    test css change

--- pages/TeamBuilder.jsx
+++ pages/TeamBuilder.jsx
@@ -5,10 +5,12 @@ export default function TeamBuilder() {
     const navigate = useNavigate();
     
     return (
-        <div className="team-builder flex flex-col items-center justify-center min-h-screen">
-            <h1 className="text-4xl font-bold mb-4">Team Builder</h1>
-            <p className="text-lg mb-8">Use this page to build your team.</p>
-            <button className="bg-blue-500 text-white px-4 py-2 rounded" onClick={() => navigate("/")}>Back to Home</button>
+        <div className="team-builder">
+            <div className="flex flex-col items-center justify-center min-h-screen bg-pink-400">
+                <h1 className="text-4xl font-bold mb-4">Team Builder</h1>
+                <p className="text-lg mb-8">Use this page to build your team.</p>
+                <button className="bg-blue-500 text-white px-4 py-2 rounded" onClick={() => navigate("/")}>Back to Home</button>
+            </div>
         </div>
     );
 }
